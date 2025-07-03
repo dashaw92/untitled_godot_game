022004b573f9b419c560ecc99f4fb3234c2e12a8
dashaw92 <dashaw92@gmail.com>
2025-07-03 13:16:03 -0400

    Init repo

---
+++ scripts/player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody2D
+
+const SPEED = 75
+enum Facing {
+	left = 0,
+	right = 1,
+	front = 2,
+	back = 3,
+}
+const FacingToString: Array = [
+	"left", "right", "front", "back"
+]
+
+var changedDirection = false
+var facing: Facing = Facing.front
+var walking = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var dx = Input.get_axis("ui_left", "ui_right")
+	var dy = Input.get_axis("ui_up", "ui_down")
+	
+	if (dx == 0 and dy == 0):
+		walking = false
+	else:
+		walking = true
+		match [dx, dy]:
+			[var x, _] when x < 0:
+				facing = Facing.left
+			[var x, _] when x > 0:
+				facing = Facing.right
+			[_, var y] when y < 0:
+				facing = Facing.back
+			[_, var y] when y > 0:
+				facing = Facing.front
+	
+	velocity = Vector2(dx, dy)
+	if(abs(dx) + abs(dy) > 1):
+		velocity = velocity.normalized()
+		
+	velocity *= SPEED
+	move_and_slide()
+	
+	animate()
+
+	pass
+
+func animate() -> void:
+	var currentAnim = $AnimatedSprite2D.animation
+	var anim: String
+	
+	if (walking):
+		anim = "walk_" + FacingToString[facing]
+	else:
+		anim = "idle_" + FacingToString[facing]
+	
+	if (anim == currentAnim):
+		pass
+	$AnimatedSprite2D.play(anim)
